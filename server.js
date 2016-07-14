8ccd6056b0b796dd0d530706a5a42a8ab9394c1a
Luis Lithgow <lalithgow@gmail.com>
2016-07-14 12:05:06 -0400

    Created server for React ToDo tasks app.

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict'
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3009;
+
+// const homeRoute = require('');
+// const taskRoute = require('./routes/tasks');
+
+// app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(logger('dev'))
+
+app.get('/', (req, res)=>{
+  res.send("HOME PAGE")
+})
+
+app.route('/tasks/:id' )
+    .get((req,res)=>res.send(`showed task ${req.params.id}`))
+    .put((req,res)=>res.send(`edited task ${req.params.id}`))
+    // used PUT to post a specific id
+    .delete((req,res)=>res.send(`deleted task ${req.params.id} `))
+
+app.route('/tasks' )
+    .get((req,res)=>res.send('show tasks'))
+    .post((req,res)=>res.send('posted new task'))
+
+
+
+app.listen(PORT, ()=>{
+  console.log('Listening to server on port ' + PORT)
+})
+
+
